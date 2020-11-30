39d40abe384d9fed73043ec5eb35458f94b0f4cb
dingzhengru <qws7869vdx@gmail.com>
2020-11-30 10:00:47 +0800

    稍微調整

--- src/main.js
+++ src/main.js
@@ -1,55 +1,49 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // socket
 import io from 'socket.io-client';
 
-const host = 'http://localhost'
-const port = '80'
-const url = `${ host }:${ port }`
+// const host = 'http://localhost';
+// const port = '80';
+// const url = `${host}:${port}`;
 
-let socket = io(url)
+const socket = io();
 // let socket = io()
 
 new Vue({
-    router,
-    store,
-    vuetify,
-    beforeCreate: function() {
+  router,
+  store,
+  vuetify,
+  beforeCreate: function() {
+    // set lobby data to store
+    this.$store.dispatch('lobby/getDataAction').then(data => {});
 
-        // set lobby data to store
-        this.$store.dispatch('lobby/getDataAction')
-        .then(data => {
+    // watch lobby
+    this.$store.dispatch('lobby/setWatchDataAction');
 
-        })
+    // socket
+    socket.on('connect', () => {
+      // console.log(socket.connected, '已連線'); // true
 
-        // watch lobby
-        this.$store.dispatch('lobby/setWatchDataAction')
-
-
-        // socket
-        socket.on('connect', () => {
-            // console.log(socket.connected, '已連線'); // true
-
-            this.$store.dispatch('socket/setDataAction', socket)
-            .then(data => {
-                setTimeout(() => {
-                    this.scrollToBottom()
-                }, 1000)
-            })
-        }); 
+      this.$store.dispatch('socket/setDataAction', socket).then(data => {
+        setTimeout(() => {
+          this.scrollToBottom();
+        }, 1000);
+      });
+    });
+  },
+  methods: {
+    scrollToBottom: function() {
+      let scrollingElement = document.scrollingElement || document.body;
+      scrollingElement.scrollTop = scrollingElement.scrollHeight;
     },
-    methods: {
-        scrollToBottom: function() {
-            let scrollingElement = document.scrollingElement || document.body;
-            scrollingElement.scrollTop = scrollingElement.scrollHeight;
-        }
-    },
-    render: h => h(App)
-}).$mount('#app')
+  },
+  render: h => h(App),
+}).$mount('#app');
