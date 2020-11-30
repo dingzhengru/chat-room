1e3fa2840d050be1cc0c8648d53bf01ca038bcce
dingzhengru <qws7869vdx@gmail.com>
2020-11-30 10:20:56 +0800

    調整

---
+++ dist/precache-manifest.bb8d21c7d417e124dcdbd031d70feb17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f956c8e27a079165a3a1580fc3dd24",
+    "url": "/chat-icon-32x32.jpg"
+  },
+  {
+    "revision": "f9bff682e8a07e9b827d",
+    "url": "/css/app.8230f62e.css"
+  },
+  {
+    "revision": "94b2906294f738e8e78b",
+    "url": "/css/chunk-vendors.05ac10a3.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon - 複製.ico"
+  },
+  {
+    "revision": "2108b5405411a00d0128672b86b7a901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9bff682e8a07e9b827d",
+    "url": "/js/app.cca98dde.js"
+  },
+  {
+    "revision": "94b2906294f738e8e78b",
+    "url": "/js/chunk-vendors.af528070.js"
+  },
+  {
+    "revision": "2a14b45352ec49712797037c11ff2b79",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
